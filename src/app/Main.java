1dbba585950dbb1441e44b207e20f7b787dfe6a1
Winston Chan <wins.chan@mail.utoronto.ca>
2023-10-28 19:53:04 -0400

    made accessobj compliant with stockprice entity
    tested each stockprice param to check for correctness

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,12 @@ import data_access.*;
 public class Main {
     public static void main(String[] args) {
         APIDataAccess api = new APIDataAccess("AMZN", "1day");
-
+        System.out.println(api.getStockPrice().getDate());
+        System.out.println(api.getStockPrice().getHigh());
+        System.out.println(api.getStockPrice().getLow());
+        System.out.println(api.getStockPrice().getVolume());
+        System.out.println(api.getStockPrice().getOpen());
+        System.out.println(api.getStockPrice().getClose());
+        //TODO also remove later; for testing
     }
 }
