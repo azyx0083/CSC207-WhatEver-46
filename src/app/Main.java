d3f1db4b822b0c0bd7730113f9160232a2cdaad9
azyx0083 <azyx0083@gmail.com>
2023-10-30 20:06:26 -0400

    add tabular view to Main and create SingleStockUseCaseFactory to test the table display

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,13 @@
 package app;
 
 
+import data_access.APIDataAccess;
+import entity.Stock;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.single_stock.SingleStockController;
+import interface_adapter.single_stock.SingleStockTabularViewModel;
+import interface_adapter.single_stock.SingleStockViewModel;
+import use_case.single_stock.SingleStockAPIDataAccessInterface;
 import  view.*;
 import javax.swing.*;
 import java.awt.*;
@@ -11,16 +18,31 @@ public class Main {
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         CardLayout cardLayout = new CardLayout();
+
+        APIDataAccess apiDataAccess = new APIDataAccess();
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+
+        // Initialize the table view, will replace this part with the input field in Menu
+        String symbol = "AMZN";
+        String name = apiDataAccess.validSymbol(symbol);
+        Stock stock = apiDataAccess.getStockData(name, symbol);
+
         JPanel views = new JPanel(cardLayout);
         application.add(views);
+        SingleStockTabularViewModel singleStockTabularViewModel = new SingleStockTabularViewModel(name, symbol,
+                stock.getHistoricalPrices(), stock.getHistoricalDates());
+
         SingleStockGraphicalView graphicalView = new SingleStockGraphicalView();
-        SingleStockTabularView tabularView = new SingleStockTabularView();
+
+        SingleStockTabularView singleStockTabularView = SingleStockUseCaseFactory.createTabular(viewManagerModel,
+                singleStockTabularViewModel, apiDataAccess);
+
         MenuView menuView = new MenuView();
-        views.add(graphicalView, graphicalView.viewName);
-        views.add(tabularView,tabularView.viewName);
-        views.add(menuView, menuView.viewName);
+//        views.add(graphicalView, graphicalView.viewName);
+        views.add(singleStockTabularView,singleStockTabularView.viewName);
+//        views.add(menuView, menuView.viewName);
         application.pack();
         application.setVisible(true);
-
     }
 }
