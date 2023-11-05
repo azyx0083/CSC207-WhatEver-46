f199d3abf469d3f852a4ff69215499c936bee887
azyx0083 <azyx0083@gmail.com>
2023-11-04 21:45:49 -0400

    make a separate SingleStockTabularOutputBoundary

--- src/use_case/single_stock/tabular/SingleStockTabularOutputBoundary.java
+++ src/use_case/single_stock/tabular/SingleStockTabularOutputBoundary.java
@@ -1,8 +1,5 @@
 package use_case.single_stock.tabular;
 
-import use_case.single_stock.SingleStockOutputData;
-import use_case.single_stock.graphical.SingleStockGraphicalOutputData;
-
 public interface SingleStockTabularOutputBoundary {
     void prepareView(SingleStockTabularOutputData data);
 }
