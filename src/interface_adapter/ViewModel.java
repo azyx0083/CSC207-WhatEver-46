4f3b1a5c1204f5d6823ff6f81c7dc1c786540b01
azyx0083 <azyx0083@gmail.com>
2023-11-05 01:26:32 -0500

    add default font style as static variables

--- src/interface_adapter/ViewModel.java
+++ src/interface_adapter/ViewModel.java
@@ -1,9 +1,13 @@
 package interface_adapter;
 
+import java.awt.*;
 import java.beans.PropertyChangeListener;
 
 public abstract class ViewModel {
     private String viewName;
+    public final static Font font1 = new Font("Serif", Font.BOLD, 18);
+    public final static Font font2 = new Font("Serif", Font.BOLD, 20);
+    public final static Font font3 = new Font("SansSerif", Font.PLAIN, 14);
 
     public ViewModel(String viewName) {
         this.viewName = viewName;
