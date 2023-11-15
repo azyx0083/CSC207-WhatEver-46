18c25e3e6fa353f1507e5d74fe68f42c1f666765
azyx0083 <azyx0083@gmail.com>
2023-11-15 00:08:28 -0500

    Include comment and JavaDoc for SingleStockOutputBoundary

--- src/use_case/single_stock/SingleStockOutputBoundary.java
+++ src/use_case/single_stock/SingleStockOutputBoundary.java
@@ -1,5 +1,12 @@
 package use_case.single_stock;
 
 public interface SingleStockOutputBoundary {
+    // Two different implementation to trigger a visualization of our data
+    // Following the strategy design pattern
+
+    /**
+     * prepare a visualization of the stock using the data provided
+     * @param data the data of a stock that required for visualization
+     */
     void prepareView(SingleStockOutputData data);
 }
