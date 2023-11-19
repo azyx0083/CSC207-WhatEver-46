2d767b43652b4169630d6d4dc063dd2a68b40064
charles <charles.feng@mail.utoronto.ca>
2023-11-19 15:56:26 -0500

    changed some implementation for getter in StockPriceDataset

--- src/interface_adapter/single_stock/graphical/StockPriceDataset.java
+++ src/interface_adapter/single_stock/graphical/StockPriceDataset.java
@@ -5,13 +5,14 @@ import org.jfree.data.general.DatasetChangeListener;
 import org.jfree.data.general.DatasetGroup;
 import org.jfree.data.time.ohlc.OHLCSeriesCollection;
 import org.jfree.data.xy.DefaultHighLowDataset;
+import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.OHLCDataset;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-public class StockPriceDataset implements OHLCDataset{
+public class StockPriceDataset extends DefaultXYDataset implements OHLCDataset{
     private HashMap<String, Object[]> data;
     private OHLCSeriesCollection dataset = new OHLCSeriesCollection();
 
@@ -25,103 +26,103 @@ public class StockPriceDataset implements OHLCDataset{
 
     /**
      * get the data and cast the type to Number from the initialized map with key "high"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type Number
      */
     @Override
     public Number getHigh(int i, int i1) {
-        return (Number) data.get("high")[i];
+        return (Number) data.get("high")[0];
     }
     /**
      * get the data and cast the type to double from the initialized map with key "high"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type double
      */
     @Override
     public double getHighValue(int i, int i1) {
-        return (double)data.get("high")[i];
+        return (double)data.get("high")[0];
     }
     /**
      * get the data and cast the type to Number from the initialized map with key "low"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type Number
      */
     @Override
     public Number getLow(int i, int i1) {
-        return (Number) data.get("low")[i];
+        return (Number) data.get("low")[0];
     }
     /**
      * get the data and cast the type to double from the initialized map with key "low"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type double
      */
     @Override
     public double getLowValue(int i, int i1) {
-        return (double) data.get("low")[i];
+        return (double) data.get("low")[0];
     }
     /**
      * get the data and cast the type to Number from the initialized map with key "open"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type Number
      */
     @Override
     public Number getOpen(int i, int i1) {
-        return (Number) data.get("open")[i];
+        return (Number) data.get("open")[0];
     }
     /**
      * get the data and cast the type to double from the initialized map with key "open"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type double
      */
     @Override
     public double getOpenValue(int i, int i1) {
-        return (double) data.get("open")[i];
+        return (double) data.get("open")[0];
     }
     /**
      * get the data and cast the type to Number from the initialized map with key "close"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type Number
      */
     @Override
     public Number getClose(int i, int i1) {
-        return (Number) data.get("close")[i];
+        return (Number) data.get("close")[0];
     }
     /**
      * get the data and cast the type to double from the initialized map with key "close"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type double
      */
     @Override
     public double getCloseValue(int i, int i1) {
-        return (double) data.get("close")[i];
+        return (double) data.get("close")[0];
     }
     /**
      * get the data and cast the type to Number from the initialized map with key "volume"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type Number
      */
     @Override
     public Number getVolume(int i, int i1) {
-        return (Number) data.get("volume")[i];
+        return (Number) data.get("volume")[0];
     }
     /**
      * get the data and cast the type to double from the initialized map with key "volume"
-     * @param i the index of needed data
-     * @param i1 default placeholder
+     * @param i default param
+     * @param i1 default param
      * @return data with type double
      */
     @Override
     public double getVolumeValue(int i, int i1) {
-        return (double) data.get("volume")[i];
+        return (double) data.get("volume")[0];
     }
 
     @Override
