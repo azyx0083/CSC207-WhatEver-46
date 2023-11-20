065ae4fa979a0692b168097662becbd7f6095751
Winston Chan <wins.chan@mail.utoronto.ca>
2023-11-20 13:52:40 -0500

    javadoc for menu presenter and viewmodel

--- src/interface_adapter/menu/MenuPresenter.java
+++ src/interface_adapter/menu/MenuPresenter.java
@@ -8,10 +8,19 @@ public class MenuPresenter implements MenuOutputBoundary {
     final MenuViewModel menuViewModel;
     ViewManagerModel viewManagerModel;
 
+    /**
+     * Constructor class. Commits to memory the view models associated with menu.
+     * @param viewManagerModel view manager model for all views
+     * @param menuViewModel view model for menu
+     */
     public MenuPresenter(ViewManagerModel viewManagerModel, MenuViewModel menuViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.menuViewModel = menuViewModel;
     }
+
+    /**
+     * Changes active view to menu view and fires property changed.
+     */
     @Override
     public void prepareMenuView() {
         viewManagerModel.setActiveView(menuViewModel.getViewName());
--- src/interface_adapter/menu/MenuViewModel.java
+++ src/interface_adapter/menu/MenuViewModel.java
@@ -7,14 +7,26 @@ public class MenuViewModel {
 
     private MenuState state = new MenuState();
 
+    /**
+     * Assigns a MenuState to the view model.
+     * @param state A MenuState object to be associated with the current view
+     */
     public void setState(MenuState state) {
         this.state = state;
     }
 
+    /**
+     * Returns the MenuState associated with the view model.
+     * @return MenuState object stored
+     */
     public MenuState getState() {
         return state;
     }
 
+    /**
+     * Gets the view name.
+     * @return the view name
+     */
     public String getViewName() {
         return "menu";
     }
