3b95505bb48f8f43a6046ff3a4232cbd37d8bea7
z.q.andy2002 <Genetorize@outlook.com>
2023-11-21 21:14:58 -0500

    Adding details for SearchState and SearchViewModel

--- src/interface_adapter/search/SearchState.java
+++ src/interface_adapter/search/SearchState.java
@@ -2,20 +2,40 @@ package interface_adapter.search;
 
 public class SearchState {
     private String name = "";
+    private String nameError = null;
     private String symbol = "";
+    private String symbolError = null;
+    private String userName = "";
+    private String userNameError = null;
 
     public SearchState(SearchState copy) {
         name = copy.name;
+        nameError = copy.nameError;
         symbol = copy.symbol;
+        symbolError = copy.symbolError;
     }
 
     public SearchState() {}
 
+    // The getters
     public String getName() {
         return name;
     }
-
     public String getSymbol() {
         return symbol;
+    }
+    public String getNameError() {
+        return nameError;
+    }
+    public String getSymbolError() {
+        return symbolError;
+    }
+
+    // The Setters
+    public String toString() {
+        return "SearchState{" +
+                "name='" + name + '\'' +
+                ", symbol='" + symbol + '\'' +
+                '}';
     }
 }
--- src/interface_adapter/search/SearchViewModel.java
+++ src/interface_adapter/search/SearchViewModel.java
@@ -2,25 +2,23 @@ package interface_adapter.search;
 
 import interface_adapter.ViewModel;
 
+import java.awt.*;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class SearchViewModel extends ViewModel {
     public static String TABLE_BUTTON_LABEL = "Table";
-
     public static String GRAPH_BUTTON_LABEL = "Graph";
-
     private SearchState state = new SearchState();
-
     public SearchViewModel() {
         super("Search");
     }
-
     public void setState(SearchState state) {
         this.state = state;
     }
-
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    public static final Font font1 = new Font("Serif", Font.BOLD, 18);
+    public static final Font font2 = new Font("Serif", Font.PLAIN, 18);
 
     // This is what the Search Presenter will call to let the ViewModel know
     // to alert the View
