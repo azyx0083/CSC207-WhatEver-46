7eb517d80b946d7bcf1d0ad23710b932560d467b
charles <charles.feng@mail.utoronto.ca>
2023-11-28 19:18:52 -0500

    Finish implementing use_case module of login method

--- src/data_access/FileDataAccess.java
+++ src/data_access/FileDataAccess.java
@@ -1,16 +1,24 @@
 package data_access;
 
+import entity.DefaultUser;
 import entity.User;
 import use_case.logged_in.LoggedInDataAccessInterface;
-import use_case.login.LoginDataAccessInterface;
+import use_case.login.LoginUserDataAccessInterface;
 import use_case.signup.SignupDataAccessInterface;
 
-public class FileDataAccess implements SignupDataAccessInterface, LoginDataAccessInterface, LoggedInDataAccessInterface {
+public class FileDataAccess implements SignupDataAccessInterface, LoginUserDataAccessInterface, LoggedInDataAccessInterface {
     public void save(User user){}
+
+    @Override
+    public User get(String username) {
+        return new DefaultUser();
+    }
 
     @Override
     public boolean existsByName(String username) {
         return username.isEmpty();
     }
+
+
 
 }
