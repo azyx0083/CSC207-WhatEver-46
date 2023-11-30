29274d00929f3240925641ac9e165f6ff7882763
Winston Chan <wins.chan@mail.utoronto.ca>
2023-11-30 17:06:00 -0500

    linked menu to search

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,6 +1,7 @@
 package view;
 
 import interface_adapter.menu.*;
+import interface_adapter.search.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,8 +16,8 @@ public class MenuView extends JPanel{
     public final String viewName = "menu";
     final MenuController menuController;
     final MenuViewModel menuViewModel;
-    //final SearchController searchController; TODO uncomment once implemented
-    //final SearchViewModel searchViewModel;
+    final SearchController searchController;
+    final SearchViewModel searchViewModel;
     private final JTextField searchInputField = new JTextField(20);
     private final JButton search;
 
@@ -27,9 +28,11 @@ public class MenuView extends JPanel{
      * /@param searchController The associated SearchController
      * /@param searchViewModel The associated SearchViewModel
      */
-    public MenuView(MenuController menuController, MenuViewModel menuViewModel/*, SearchController searchController, SearchViewModel searchViewModel*/){
+    public MenuView(MenuController menuController, MenuViewModel menuViewModel, SearchController searchController, SearchViewModel searchViewModel){
         this.menuController = menuController;
         this.menuViewModel = menuViewModel;
+        this.searchController = searchController;
+        this.searchViewModel = searchViewModel;
 
         this.setPreferredSize(new Dimension(400,200));
 
@@ -48,7 +51,7 @@ public class MenuView extends JPanel{
                         if (e.getSource().equals(search)) {
                             MenuState menuState = menuViewModel.getState();
 
-                            //searchController.executeStockSearch(menuState.getStockSymbol()); TODO uncomment after merge
+                            searchController.execute(menuState.getStockSymbol());
                         }
                     }
                 }
