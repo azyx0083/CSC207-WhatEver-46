b448c334d9f02ce1c49ed173d90e8fc54a056b0e
charles <charles.feng@mail.utoronto.ca>
2023-12-01 17:23:53 -0500

    Add two test class

--- test/use_case/login/LoginTest.java
+++ test/use_case/login/LoginTest.java
@@ -1,9 +1,10 @@
-package use_case;
+package use_case.login;
+import data_access.APIDataAccess;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
-
+    static APIDataAccess apiDataAccess;
 }
--- test/use_case/signup/SignupTest.java
+++ test/use_case/signup/SignupTest.java
@@ -1,9 +1,10 @@
-package use_case;
+package use_case.signup;
+import data_access.APIDataAccess;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SignupTest {
-
+    static APIDataAccess apiDataAccess;
 }
