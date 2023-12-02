f793031359db6dca5333bda5fba3a9fc1e9fb914
Winston Chan <wins.chan@mail.utoronto.ca>
2023-12-02 16:25:10 -0500

    settingsinputdata filled

--- src/use_case/settings/SettingsInputData.java
+++ src/use_case/settings/SettingsInputData.java
@@ -1,4 +1,24 @@
 package use_case.settings;
 
 public class SettingsInputData {
+    final private String interval;
+    final private int dataSize;
+    final private String[] favorites;
+    public SettingsInputData(String interval, int dataSize, String[] favorites) {
+        this.interval = interval;
+        this.dataSize = dataSize;
+        this.favorites = favorites;
+    }
+
+    public String getInterval() {
+        return interval;
+    }
+
+    public int getDataSize() {
+        return dataSize;
+    }
+
+    public String[] getFavorites() {
+        return favorites.clone(); // Return a separate object to prevent modification
+    }
 }
