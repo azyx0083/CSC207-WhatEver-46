cd5e6820c794220656c5fea8cfa443757eebae74
Winston Chan <wins.chan@mail.utoronto.ca>
2023-12-03 01:02:13 -0500

    gave concrete interval list

--- src/view/SettingsView.java
+++ src/view/SettingsView.java
@@ -42,7 +42,6 @@ public class SettingsView extends JPanel {
                             settingsController.applyChanges(
                                     settingsState.getInterval(),
                                     settingsState.getDataSize(),
-                                    settingsState.getFavorites(),
                                     settingsState.getUsername());
                         }
                     }
@@ -60,7 +59,7 @@ public class SettingsView extends JPanel {
                 }
         );
 
-        String[] options = {"Option 1", "Option 2", "Option 3", "Option 4"};
+        String[] options = {"1min", "5min", "15min", "30min", "45min", "1h", "2h", "4h", "1day", "1week", "1month"};
         JComboBox<String> intervalDropdown = new JComboBox<>(options);
         intervalDropdown.addActionListener(new ActionListener() {
             @Override
