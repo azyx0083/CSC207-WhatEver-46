e838eecd1edf7213034b59f2307aacf46bd60f46
Winston Chan <wins.chan@mail.utoronto.ca>
2023-12-03 18:37:19 -0500

    filled menuinteractor test

--- test/use_case/menu/MenuInteractorTest.java
+++ test/use_case/menu/MenuInteractorTest.java
@@ -1,4 +1,37 @@
 package use_case.menu;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.menu.MenuPresenter;
+import interface_adapter.menu.MenuViewModel;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MenuInteractorTest {
+    static MenuPresenter presenter;
+    static MenuViewModel viewModel;
+    static MenuInteractor interactor;
+    static ViewManagerModel manager;
+
+    @BeforeAll
+    static void setup() {
+        viewModel = new MenuViewModel();
+        manager = new ViewManagerModel();
+        presenter = new MenuPresenter(manager, viewModel);
+        interactor = new MenuInteractor(presenter);
+    }
+
+    @Test
+    void testSwitchToMenu() {
+        interactor.prepareMenuView();
+        assertEquals(manager.getActiveView(), viewModel.getViewName());
+    }
+
+    @Test
+    void testSwitchToMenuWithUsername() {
+        interactor.prepareMenuView("John");
+        assertEquals(manager.getActiveView(), viewModel.getViewName());
+        assertEquals(viewModel.getState().getUsername(), "John");
+    }
 }
