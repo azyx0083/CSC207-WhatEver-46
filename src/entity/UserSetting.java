2dcb600f168c9b1c524715ddc63d09c2025a0cbe
azyx0083 <azyx0083@gmail.com>
2023-12-03 14:38:46 -0500

    Make User store UserSetting instead of interval and OutputSize

--- src/entity/UserSetting.java
+++ src/entity/UserSetting.java
@@ -1,20 +1,12 @@
 package entity;
 
-import java.util.Map;
-
 public class UserSetting {
-    private final Map<String, Stock> favouriteStocks;
     private String interval;
     private int outputSize;
 
-    public UserSetting(Map<String, Stock> favouriteStocks, String interval, int outputSize) {
-        this.favouriteStocks = favouriteStocks;
+    public UserSetting(String interval, int outputSize) {
         this.interval = interval;
         this.outputSize = outputSize;
-    }
-
-    public Map<String, Stock> getFavouriteStocks() {
-        return favouriteStocks;
     }
 
     public String getInterval() {
@@ -31,16 +23,6 @@ public class UserSetting {
 
     public void setOutputSize(int outputSize) {
         this.outputSize = outputSize;
-    }
-
-    public void addFavouriteStock(Stock stock) {
-        favouriteStocks.put(stock.getSymbol(), stock);
-    }
-
-    public void reset() {
-        for (Stock stock : favouriteStocks.values()) {
-            stock.reset();
-        }
     }
 
 }
