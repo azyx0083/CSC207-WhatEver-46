41036f36d18c1b529dad18704a2220cc3ca1bf00
azyx0083 <azyx0083@gmail.com>
2023-12-03 14:38:04 -0500

    Mack UserFactory responsible for default user creation instead of separate class for DefaultUser

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -1,28 +1,15 @@
 package entity;
 
-import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserFactory {
-    public static User createUser(String username, String password, String interval, int outputSize, JSONObject favouriteStocks) {
+    public static User createUser(String username, String password, String interval, int outputSize) {
         Map<String, Stock> stocks = new HashMap<>();
-        for (String symbol : favouriteStocks.keySet()) {
-            JSONObject object = favouriteStocks.getJSONObject(symbol);
-            Stock stock = new Stock(object.getString("symbol"));
-            stock.setInfo(object.getString("name"), object.getString("exchange"),
-                    object.getString("currency"), object.getString("country"), object.getString("type"));
-            stocks.put(symbol, stock);
-        }
-        return new User(username, password, new UserSetting(stocks, interval, outputSize));
+        return new User(username, password, new UserSetting(interval, outputSize));
     }
 
-    public static User createUser(String username, String password, String interval, int outputSize, Map<String, Stock> favouriteStocks) {
-        return new User(username, password, new UserSetting(favouriteStocks, interval, outputSize));
-    }
-
-    public static DefaultUser createDefaultUser() {
-        return new DefaultUser();
+    public static User createDefaultUser() {
+        return new User(null, null, new UserSetting("1day", 30));
     }
 }
