f8a67c58027d926ab36ce3d796d94a85797b7ffc
z.q.andy2002 <Genetorize@outlook.com>
2023-12-06 21:40:56 -0500

    Revert "Modified LoginViewTest"
    This reverts commit bc5270840aecc3ea11d874f6c9fed0b70a1cfc41.

--- test/view/LoginViewTest.java
+++ test/view/LoginViewTest.java
@@ -1,25 +1,24 @@
 package view;
 
+import app.factory.LoginUseCaseFactory;
 import data_access.FileUserDataAccess;
 import entity.User;
 import entity.UserFactory;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.login.LoginController;
-import interface_adapter.login.LoginPresenter;
 import interface_adapter.login.LoginState;
 import interface_adapter.login.LoginViewModel;
 import interface_adapter.menu.MenuController;
-import interface_adapter.menu.MenuPresenter;
 import interface_adapter.menu.MenuViewModel;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import use_case.login.LoginInteractor;
+import use_case.login.LoginInputBoundary;
 import use_case.menu.MenuInputBoundary;
-import use_case.menu.MenuInteractor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginViewTest {
+    static LoginInputBoundary lib;
     static LoginViewModel loginViewModel;
     static LoginController loginController;
     static MenuInputBoundary mib;
@@ -33,6 +32,14 @@ public class LoginViewTest {
     @BeforeAll
     static void setUp() {
         /*
+        lib = null;
+        loginController = new LoginController(lib);
+        loginViewModel = new LoginViewModel();
+        mib = null;
+        menuController = new MenuController(mib);
+        loginView = new LoginView(loginViewModel, loginController, menuController);
+         */
+
         fileUserDataAccess = new FileUserDataAccess("file.txt");
         sampleUser = UserFactory.createUser("sample", "password", "1day", 10);
         fileUserDataAccess.save(sampleUser);
@@ -41,28 +48,6 @@ public class LoginViewTest {
         menuViewModel = new MenuViewModel();
         loginView = LoginUseCaseFactory.createLoginView(viewManagerModel,loginViewModel,menuViewModel,
                 fileUserDataAccess);
-
-        Main.main(null);
-        fileUserDataAccess = new FileUserDataAccess("file.txt");
-        sampleUser = UserFactory.createUser("sample", "password", "1day", 10);
-        fileUserDataAccess.save(sampleUser);
-         */
-
-        fileUserDataAccess = new FileUserDataAccess("file.txt");
-        sampleUser = UserFactory.createUser("sample", "password", "1day", 10);
-        fileUserDataAccess.save(sampleUser);
-
-        loginViewModel = new LoginViewModel();
-        viewManagerModel = new ViewManagerModel();
-        menuViewModel = new MenuViewModel();
-        LoginPresenter loginPresenter = new LoginPresenter(viewManagerModel, menuViewModel, loginViewModel);
-        LoginInteractor loginInteractor = new LoginInteractor(fileUserDataAccess, loginPresenter);
-        loginController = new LoginController(loginInteractor);
-
-        MenuPresenter menuPresenter = new MenuPresenter(viewManagerModel, menuViewModel);
-        MenuInteractor menuInteractor = new MenuInteractor(menuPresenter);
-        menuController = new MenuController(menuInteractor);
-        loginView = new LoginView(loginViewModel, loginController, menuController);
     }
 
     /*
